3de09f067f84906cfc0a019f3ba4f8a37e60946f
BouncyNudibranch <bouncynudibranch@gmail.com>
2014-07-31 15:54:14 -0400

    modified bean purchase form to use BEAN_TYPES static from models

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import Form
 from wtforms import StringField, PasswordField, IntegerField, SelectField, DateField, TextAreaField
 from wtforms.validators import DataRequired, EqualTo, Optional
+from app.models import BEAN_TYPES
 
 
 class LoginForm(Form):
@@ -17,11 +18,8 @@ class RegisterForm(Form):
 
 class BeanPurchaseForm(Form):
     name = StringField('Bean Name', validators=[DataRequired()])
-    type = SelectField('Bean Type', choices=[
-        (1, 'Green'),
-        (2, 'Roasted'),
-        (3, 'Pre-ground')
-    ], coerce=int, validators=[DataRequired()])
+    type = SelectField('Bean Type', choices=[(c, BEAN_TYPES[c]) for c in BEAN_TYPES],
+                       coerce=int, validators=[DataRequired()])
     weight = IntegerField('Weight (grams)', validators=[DataRequired()])
     purchase_date = DateField('Purchase Date', validators=[DataRequired()])
     notes = TextAreaField('Notes', validators=[Optional()])
