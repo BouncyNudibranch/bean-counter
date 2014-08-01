005a0d42f65c82636c495dea2bbf5b569b20ab15
BouncyNudibranch <bouncynudibranch@gmail.com>
2014-08-01 16:36:13 -0400

    added ability to configure database and secret key via environment variables

--- config.py
+++ config.py
@@ -1,8 +1,18 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'bean_counter.db')
-SECRET_KEY = 'qwertyuiop[]'
+ENV_DB = 'BEANCOUNTER_DATABASE_URI'
+ENV_SKEY = 'BEANCOUNTER_SECRET_KEY'
+
+try:
+    SQLALCHEMY_DATABASE_URI = os.environ[ENV_DB]
+except KeyError:
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'bean_counter.db')
+
+try:
+    SECRET_KEY = os.environ[ENV_SKEY]
+except KeyError:
+    SECRET_KEY = 'qwertyuiop[]'
 
 BEANS_PER_PAGE = 20
 BREWS_PER_PAGE = 20
