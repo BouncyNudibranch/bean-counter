aa6b9bc15e4cf28d62c6a5b8c9c64a4ace4e26c3
BouncyNudibranch <bouncynudibranch@gmail.com>
2014-08-11 19:55:15 -0400

    added license and footer

--- app/views.py
+++ app/views.py
@@ -1,3 +1,22 @@
+"""
+Bean Counter - Track Your Coffee!
+Copyright (C) 2014  BonucyNudibranch (bouncynudibranch@gmail.com)
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+"""
+
 from flask import render_template, redirect, g, request, url_for
 from flask_login import current_user, login_user, logout_user, login_required
 from config import BEANS_PER_PAGE, BREWS_PER_PAGE, ROASTS_PER_PAGE
