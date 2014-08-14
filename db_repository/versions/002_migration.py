e0f2f1de265636831c6b829ac2b0dcdb94a6f45d
BouncyNudibranch <bouncynudibranch@gmail.com>
2014-08-14 16:50:05 -0400

    created migrations script for cupping model (cherry picked from commit 2024b2d)

---
+++ db_repository/versions/002_migration.py
@@ -0,0 +1,34 @@
+from sqlalchemy import *
+from migrate import *
+
+
+from migrate.changeset import schema
+pre_meta = MetaData()
+post_meta = MetaData()
+cupping = Table('cupping', post_meta,
+    Column('id', Integer, primary_key=True, nullable=False),
+    Column('datetime', DateTime),
+    Column('aroma_notes', Text),
+    Column('acidity_notes', Text),
+    Column('flavour_notes', Text),
+    Column('mouthfeel_notes', Text),
+    Column('aftertaste_notes', Text),
+    Column('overall_notes', Text),
+    Column('extra_notes', Text),
+    Column('roast_id', Integer),
+)
+
+
+def upgrade(migrate_engine):
+    # Upgrade operations go here. Don't create your own engine; bind
+    # migrate_engine to your metadata
+    pre_meta.bind = migrate_engine
+    post_meta.bind = migrate_engine
+    post_meta.tables['cupping'].create()
+
+
+def downgrade(migrate_engine):
+    # Operations to reverse the above upgrade go here.
+    pre_meta.bind = migrate_engine
+    post_meta.bind = migrate_engine
+    post_meta.tables['cupping'].drop()
